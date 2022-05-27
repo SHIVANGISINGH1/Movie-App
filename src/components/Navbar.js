ee146fdf55a809395ec0f0655c978e9157be177b
Shivangi Singh <shivangisinghhappy@gmail.com>
2022-05-28 00:11:33 +0530

    centered navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -3,20 +3,25 @@ import { Navbar, Form, Button, Container, FormControl } from "react-bootstrap";
 function Navbar_Component() {
     return (
         <Navbar expand="lg" variant="dark" bg="dark">
-            <Container >
+            <Container>
+            
             <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+         
             <Navbar.Collapse id="responsive-navbar-nav">
             <Navbar.Brand href="#">Movies</Navbar.Brand>
-            <Form className="d-flex" >
-            <FormControl type="search"
-            placeholder="Search"
-            className="me-2"
-            aria-label="Search"
-            />
-            <Button variant="danger">Search</Button>
-        </Form>
-        </Navbar.Collapse>
-        </Container>
+           
+                <Form className="d-flex">
+                    <FormControl type="search"
+                    placeholder="Search"
+                    className="me-2"
+                    aria-label="Search"
+                    />
+                    <Button variant="danger">Search</Button>
+                </Form>
+            
+            </Navbar.Collapse>
+           
+            </Container>
       </Navbar>
     );
 }
